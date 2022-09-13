f46c1740b5669175849e30f4b1f3094e4e254114
Salochin500 <110814545+Salochin500@users.noreply.github.com>
2022-09-13 10:11:01 +1000

    Create HarmonyPatches.cs

---
+++ HarmonyPatches.cs
@@ -0,0 +1,29 @@
+using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace FirstPersonCamera
+{
+    public class HarmonyPatches
+    {
+        private static Harmony instance;
+
+        public static bool IsPatched { get; private set; }
+        public const string InstanceId = PluginInfo.GUID;
+
+        internal static void ApplyHarmonyPatches()
+        {
+            if (!IsPatched)
+            {
+                if (instance == null)
+                {
+                    instance = new Harmony(InstanceId);
+                }
+
+                instance.PatchAll(Assembly.GetExecutingAssembly());
+                IsPatched = true;
+            }
+        }
+    }
+
+}
